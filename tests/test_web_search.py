7b20656cf7b27d653c7520fbe5144f03c23a0d8a
rtjamidar <rtjamidar@gmail.com>
2024-03-06 02:59:14 +0530

    added execute python code

--- tests/test_web_search.py
+++ tests/test_web_search.py
@@ -5,7 +5,7 @@ from tools.web_search import web_search
 class TestWebSearch(TestCase):
     def test_web_search(self):
         num_results = 2
-        _results = web_search("who are c suite exectuives for dell?", num_results)
-        self.assertEqual(len(_results) == num_results)
-        self.assertIn( "title" , _results[0])
+        _results = web_search("what is nyse cxm price today", num_results)
+        self.assertEqual(len(_results), num_results)
+        self.assertIn("title", _results[0])
         print(_results)
