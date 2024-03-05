0b8121d168242e54b746ae304c0f1c3fdbc2d5fc
Mayank Musaddi <mayank.musaddi@sprinklr.com>
2024-03-05 19:30:35 +0530

    init: base setup

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import tornado
+PORT = 10000
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class BaseHandler(tornado.web.RequestHandler):
+    def __init__(self):
+        super().set_header('Content-Type', 'application/json')
+
+    async def post(self):
+        payload = tornado.escape.json_decode(self.request.body)
+        response = await self.process(payload)
+        return response
+
+    async def process(self, payload):
+        return {"success": True}
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm1')
+def main(port=PORT):
+    base_handler = BaseHandler()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = tornado.web.Application(handlers=[("/predict", base_handler)])
+    app.listen(port)
+
+
+if __name__ == "__main__":
+    main()
