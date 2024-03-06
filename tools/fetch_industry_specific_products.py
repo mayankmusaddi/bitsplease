5029e1fe03ca18f641afed53da0b0f2e6d511853
Vikram Naresh <vikramnaresh@gmail.com>
2024-03-06 20:05:26 +0530

    Create fetch_industry_specific_products.py

---
+++ tools/fetch_industry_specific_products.py
@@ -0,0 +1,5 @@
+async def fetch_industry_specific_products(vertical: str):
+    """
+    Returns all of Sprinklr's products that can be pitched to a company under the specified vertical.
+    """
+    
