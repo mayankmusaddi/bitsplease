004b8332c34e36413bb37dbfe06baac01005095c
Vikram Naresh <vikramnaresh@gmail.com>
2024-03-06 21:00:26 +0530

    Create db_store.py

---
+++ utils/db_crud.py
@@ -0,0 +1,38 @@
+class StoreDB:
+    def __init__(self, db_file):
+        import json
+        import os
+        self.db_file = db_file
+        if os.path.exists(db_file):
+            with open(db_file, 'r') as f:
+                self.db = json.load(f)
+        else:
+            self.db = {}
+
+    def create(self, key, value):
+        if key in self.db:
+            raise Exception('Key already exists')
+        self.db[key] = value
+        self._persist()
+
+    def read(self, key):
+        try:
+            return self.db[key]
+        except KeyError:
+            raise Exception('Key not found')
+
+    def update(self, key, value):
+        if key not in self.db:
+            raise Exception('Key not found')
+        self.db[key] = value
+        self._persist()
+
+    def delete(self, key):
+        if key not in self.db:
+            raise Exception('Key not found')
+        del self.db[key]
+        self._persist()
+
+    def _persist(self):
+        with open(self.db_file, 'w') as f:
+            json.dump(self.db, f)
