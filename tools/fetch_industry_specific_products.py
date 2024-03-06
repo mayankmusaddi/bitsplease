bf11b3b409d3d9ded161ecc9ece8712f8b042f87
Vikram Naresh <vikramnaresh@gmail.com>
2024-03-06 20:18:26 +0530

    Update fetch_industry_specific_products.py

--- tools/fetch_industry_specific_products.py
+++ tools/fetch_industry_specific_products.py
@@ -4,11 +4,11 @@ async def fetch_industry_specific_products(vertical: str):
     """
     import aiofiles
     import ast
-    async with aiofiles.open('../kb/spr_products_short.txt', mode='r') as file:
+    async with aiofiles.open('../kb/vertical_product_map.txt', mode='r') as file:
         contents = file.read()
-
-    return ast.literal_eval(str(contents))
+    mapping = ast.literal_eval(str(contents))
+    
     system_prompt = ("You are an expert in understanding large data and how to answer queries based on it. You help in understanding which CRM/CXM products can be pitched to clients.")
-    messages = [{"role": "user", "content": f"This is a mapping of Sprinklr products can be suggested to companies of specific vertical industries: {mapping}"}]
+    messages = [{"role": "user", "content": f"This is a mapping of Sprinklr products can be suggested to companies of specific vertical industries: {mapping}.\n Your response should only be the list of products that can be pitched to a company belonging to the {vertical} industry."}]
     _response, _ = await openai_call(system_prompt, messages)
     return _response["results"]
