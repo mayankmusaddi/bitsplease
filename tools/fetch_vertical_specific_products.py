c35fa45106bea03b2b3ffb7f59f9a04dd0f51a8d
Vikram Naresh <vikramnaresh@gmail.com>
2024-03-06 21:49:20 +0530

    Update fetch_vertical_specific_products.py

--- tools/fetch_vertical_specific_products.py
+++ tools/fetch_vertical_specific_products.py
@@ -3,15 +3,13 @@ async def fetch_vertical_specific_products(vertical: str):
     Returns all of Sprinklr's products that can be pitched to a company under the specified vertical.
     """
     import aiofiles
-    import ast
     import os
     from utils.openai_utils import openai_call
 
     current_dir = os.getcwd()
     file_path = os.path.join(current_dir, '../kb/vertical_product_map.txt')
     async with aiofiles.open(file_path, mode='r') as file:
-        contents = await file.read()
-    mapping = ast.literal_eval(str(contents))
+        mapping = await file.read()
     
     system_prompt = ("You are an expert in understanding large data and how to answer queries based on it. You help in understanding which CRM/CXM products can be pitched to clients.")
     messages = [{"role": "user", "content": f"This is a mapping of Sprinklr products can be suggested to companies of specific vertical industries: {mapping}.\n Your response should only be the list of products that can be pitched to a company belonging to the {vertical} industry."}]
