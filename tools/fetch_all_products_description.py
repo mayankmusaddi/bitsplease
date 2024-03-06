c42e5ac8a0dd6f6e709bdd6cb0b933e6a8739e53
Vikram Naresh <vikramnaresh@gmail.com>
2024-03-06 20:07:34 +0530

    aio file read

--- tools/fetch_all_products_description.py
+++ tools/fetch_all_products_description.py
@@ -2,9 +2,10 @@ async def fetch_all_products_description():
     """
     Function to return few-line description of all products offered by Sprinklr.
     """
+    import aiofiles
     import ast
     import json
-    with open('../kb/spr_products_short.txt', 'r') as file:
+    async with aiofiles.open('../kb/spr_products_short.txt', mode='r') as file:
         contents = file.read()
 
     return ast.literal_eval(str(contents))
