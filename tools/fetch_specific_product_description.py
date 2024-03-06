54c54e246842e59e59885f82027afd1a34b7cc90
Vikram Naresh <vikramnaresh@gmail.com>
2024-03-06 21:47:14 +0530

    Update fetch_specific_product_description.py

--- tools/fetch_specific_product_description.py
+++ tools/fetch_specific_product_description.py
@@ -4,9 +4,13 @@ async def fetch_specific_product_description(product: str):
     """
     import aiofiles
     import ast
+    import os
     from utils.openai_utils import openai_call
-    async with aiofiles.open('../kb/spr_products_long.txt', mode='r') as file:
-        contents = file.read()
+
+    current_dir = os.getcwd()
+    file_path = os.path.join(current_dir, '../kb/spr_products_long.txt')
+    async with aiofiles.open(file_path, mode='r') as file:
+        contents = await file.read()
     mapping = ast.literal_eval(str(contents))
     
     system_prompt = ("You are an expert in understanding large data and answering queries based on it.")
